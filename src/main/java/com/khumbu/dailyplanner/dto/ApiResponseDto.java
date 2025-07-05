b27d612084b82ff0f993dea2ffed7c0541d5b12d
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-07-04 20:08:31 -0500

    add ApiResponseDto

---
+++ src/main/java/com/khumbu/dailyplanner/dto/ApiResponseDto.java
@@ -0,0 +1,81 @@
+package com.khumbu.dailyplanner.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class ApiResponseDto<T> {
+
+   private  LocalDateTime time;
+   private String message;
+   private HttpStatus httpStatus;
+   private String responseCode;
+   private T t;
+   private  Exception exception;
+
+    public ApiResponseDto() {
+    }
+
+    public ApiResponseDto(T t, String message, HttpStatus httpStatus) {
+        this.t = t;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public ApiResponseDto(LocalDateTime time, String message, HttpStatus httpStatus, String responseCode, T t, Exception exception) {
+        this.time = time;
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.responseCode = responseCode;
+        this.t = t;
+        this.exception = exception;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public T getData() {
+        return t;
+    }
+
+    public void setData(T t) {
+        this.t = t;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+}
