64c2cd5649cd69b469575621c94542dd59457813
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-07-04 20:16:12 -0500

    add DayDto

--- src/main/java/com/khumbu/dailyplanner/models/DayDto.java
+++ src/main/java/com/khumbu/dailyplanner/models/DayDto.java
@@ -3,16 +3,17 @@ package com.khumbu.dailyplanner.models;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class DayDto {
     private Long id;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
 
 
@@ -22,7 +23,14 @@ public class DayDto {
 
     public static List<DayDto> createList(List<Day> dayList){
 
-       List<DayDto> dayDtoList= dayList.stream().map(day->DayDto.create(day)).toList();
+       List<DayDto> dayDtoList= dayList.stream().map(DayDto::create).toList();
         return dayDtoList;
+    }
+
+    public Day getDay(){
+        Day day = new Day();
+        day.setId(id);
+        day.setDate(date);
+        return day;
     }
 }
