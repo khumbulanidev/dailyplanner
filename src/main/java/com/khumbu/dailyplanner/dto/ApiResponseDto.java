6e81a236c3fb9ef97ded5169114ef921ddb9019b
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-07-17 21:22:58 -0500

    Refactor ApiResponseDto

--- src/main/java/com/khumbu/dailyplanner/dto/ApiResponseDto.java
+++ src/main/java/com/khumbu/dailyplanner/dto/ApiResponseDto.java
@@ -13,6 +13,8 @@ public class ApiResponseDto<T> {
    private T t;
    private  Exception exception;
 
+   private String token;
+
     public ApiResponseDto() {
     }
 
@@ -20,6 +22,16 @@ public class ApiResponseDto<T> {
         this.t = t;
         this.message = message;
         this.httpStatus = httpStatus;
+    }
+
+    public ApiResponseDto(LocalDateTime time, String message, HttpStatus httpStatus, String responseCode, T t, Exception exception, String token) {
+        this.time = time;
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.responseCode = responseCode;
+        this.t = t;
+        this.exception = exception;
+        this.token = token;
     }
 
     public ApiResponseDto(LocalDateTime time, String message, HttpStatus httpStatus, String responseCode, T t, Exception exception) {
@@ -77,5 +89,35 @@ public class ApiResponseDto<T> {
 
     public void setException(Exception exception) {
         this.exception = exception;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ApiResponseDto{" +
+                "time=" + time +
+                ", message='" + message + '\'' +
+                ", httpStatus=" + httpStatus +
+                ", responseCode='" + responseCode + '\'' +
+                ", t=" + t +
+                ", exception=" + exception +
+                ", token='" + token + '\'' +
+                '}';
     }
 }
