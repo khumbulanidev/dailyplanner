f84bc984ece74c69a75d96cf30f2069f9fab2389
khumbulani.ndlovu <kndlovudev@gmail.com>
2026-06-02 00:49:52 -0500

    Correct sql operation name

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert ignore into user_operation(id, is_active, position, link, operation) values(1,1,1,'calendars','Calendar');
+
+insert into user_operation(id, is_active, position, link, operation) values(2,1,1,'daily-tasks','Daily Tasks');
+
+insert into user_operation(id, is_active, position, link, operation) values(3,1,1,'days','All days');
+
+insert into user_operation(id, is_active, position, link, operation) values(4,1,1,'tasks','Tasks');
+
+insert into user_operation(id, is_active, position, link, operation) values(5,1,1,'reports','Reports');
+
+insert into user_operation(id, is_active, position, link, operation) values(6,1,1,'add-task','Add Task');
